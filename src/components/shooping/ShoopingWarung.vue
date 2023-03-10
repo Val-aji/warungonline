30b5e261982d003867c4da4853e1e1ba2abbcc69
Nouval Aji Satrio <82368499+Val-aji@users.noreply.github.com>
2023-03-10 23:11:28 +0700

    Update ShoopingWarung.vue

--- src/components/shooping/ShoopingWarung.vue
+++ src/components/shooping/ShoopingWarung.vue
@@ -39,25 +39,26 @@
 <script>
     import NavigasiBar from '../models/navigasi/NavigasiBar.vue';
     import SearchWarung from "../models/search/SearchWarung.vue"
-    import {products} from "../../data.js"
+    //import {products} from "../../data.js"
     import CardShooping from '../models/cardShooping/CardShooping.vue';
     import DarkMode from "../models/darkMode/DarkMode.vue"
     import KategoriHome from "../home/kategori/KategoriHome.vue"
     import IklanWarung from "../home/iklan/IklanHome.vue"
-
+    import {instance} from "../../../config/logic.js
     export default {
         name: "ShoopipngWarung", 
         data() {
             return {
                 tombolFilter: true,
-                filterKategori: []
+                filterKategori: [],
+                products: []
             }
         },
         watch: {
             filterKategori(resNew) {
                 const newProduk = []
                 resNew.map(item => {
-                    products.slice().filter(itemProducts => {
+                    this.products.slice().filter(itemProducts => {
                         
                         if(itemProducts.kategoriProduk.match(item)) {
                             newProduk.push(itemProducts)
@@ -78,7 +79,14 @@
             DarkMode
         },
         beforeMount() {
-            this.produk = products.slice()
+            instance().get("/produk")
+               .then(res => {
+                  this.products = res.data.data
+               }.catch(err => {
+                  console.log(err)
+               }
+             
+            this.produk = this.products.slice()
             const {kategori} = this.$route.query
             if(!kategori) {
                 return false
