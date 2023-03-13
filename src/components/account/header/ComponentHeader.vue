3244be0613d15ce8afc690af6c93433b99672861
Nouval Aji Satrio <82368499+Val-aji@users.noreply.github.com>
2023-03-13 15:27:44 +0700

    Update ComponentHeader.vue

--- src/components/account/header/ComponentHeader.vue
+++ src/components/account/header/ComponentHeader.vue
@@ -1,21 +1,17 @@
 <template>
     <div class="componentHeader">
         <div class="container">
-            <img class="gambarAccount" :src="sampelAccount.gambar_account" alt="gambar Profile" />
-            <h3 class="namaLengkap HEEBO">{{sampelAccount.namaLengkap_accout}}</h3>
+            <img class="gambarAccount" src="https://backend-warungonline-production.up.railway.app/image/Screenshot_20230205-224818_1.png" alt="gambar Profile" />
+            <h3 class="namaLengkap HEEBO">Admin warungonline</h3>
         </div>
     </div>
 </template>
 
 <script>
     import "./index.css"
-    import { sampelAccount } from "../../../data";
+    
     export default {
         name: "ComponentHeader",
-        data() {
-            return {
-                sampelAccount
-            }
-        }
+ 
     }
 </script>
